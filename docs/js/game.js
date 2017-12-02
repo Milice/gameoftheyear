2c2e514d24e5e6dc9ad8faa96ad4b5d69e9558e1
Milice <milice@live.se>
2017-12-02 19:51:02 +0100

    No overlapping trees and some ship stuff

--- docs/js/game.js
+++ docs/js/game.js
@@ -1,41 +1,115 @@
 var trees = [];
+var treesBuilt = 0;
 var points = 0;
 
+var gameWindow = null;
+
+var gameHeight = 0;
+var gameWidth = 0;
+
+var gamePosLeft = 0;
+var gamePosTop = 0;
+
+var spriteHeightTree = 64;
+var spriteWidthTree = 64;
+
+var maxLeft = 0;
+var maxTop = 0;
+
 function updatePoints(amount) {
+	var oldPoints = points;
 	points += amount;
 	document.getElementById("treepoints").innerHTML = points;
+	if(oldPoints < 25 && points >= 25) {
+		alert("You have collected enough wood!\nPress B to build a ship.");
+	}
+}
+
+function treeOverlapLeft(pos, size, currentTree) {
+	var oldStart = trees[currentTree][0];
+	var oldEnd = oldStart + size;
+
+	var newStart = pos;
+	var newEnd = pos + size;
+
+	if( (newStart <= oldEnd && newEnd >= oldEnd) || (newStart <= oldStart && newEnd >= oldStart) ) {
+		return true;
+	}
+
+	return false;
+}
+
+function treeOverlapTop(pos, size, currentTree) {
+	var oldStart = trees[currentTree][1];
+	var oldEnd = oldStart + size;
+
+	var newStart = pos;
+	var newEnd = pos + size;
+
+	if( (newStart <= oldEnd && newEnd >= oldEnd) || (newStart <= oldStart && newEnd >= oldStart) ) {
+		return true;
+	}
+
+	return false;
+}
+
+function isTreeOverlapping(posTop, posLeft, width, height) {
+	for(var i = 0; i < trees.length; i++) {
+		if(treeOverlapTop(posTop, height, i) && treeOverlapLeft(posLeft, width, i)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function generateTreePos(width, height) {
+	var positions = [];
+
+	var posLeft = (Math.floor(Math.random() * maxLeft)) + gamePosLeft;
+	var posTop = (Math.floor(Math.random() * maxTop)) + gamePosTop;
+
+	while(isTreeOverlapping(posTop, posLeft, width, height)) {
+		posLeft = (Math.floor(Math.random() * maxLeft)) + gamePosLeft;
+		posTop = (Math.floor(Math.random() * maxTop)) + gamePosTop;
+	}
+
+	positions[0] = posLeft;
+	positions[1] = posTop;
+
+	return positions;
+}
+
+function generateTree() {
+	var treePos = generateTreePos(spriteWidthTree, spriteHeightTree);
+	var posLeft = treePos[0];
+	var posTop = treePos[1];
+
+	var treeCount = trees.length;
+	var tree = [posLeft, posTop, "tree"+treesBuilt];
+	trees[treeCount] = tree;
+
+	gameWindow.innerHTML = gameWindow.innerHTML + '<img id="tree' + treesBuilt + '" src="sprites/tree.png" style="z-index: 1; position: absolute; left: ' + posLeft + 'px; top: ' + posTop + 'px"/>';
+	treesBuilt++;
 }
 
 function generateTrees() {
-	var gameWindow = document.getElementById("game");
+	gameWindow = document.getElementById("game");
 	var posInfo = gameWindow.getBoundingClientRect();
 
-	var gameHeight = posInfo.height;
-	var gameWidth = posInfo.width;
+	gameHeight = posInfo.height;
+	gameWidth = posInfo.width;
 
-	var gamePosLeft = posInfo.left;
-	var gamePosTop = posInfo.top;
+	gamePosLeft = posInfo.left;
+	gamePosTop = posInfo.top;
 
-	var spriteHeight = 64;
-	var spriteWidth = 64;
+	maxLeft = gameWidth - spriteWidthTree;
+	maxTop = gameHeight - spriteHeightTree;
 
-	var maxLeft = gameWidth - spriteWidth;
-	var maxTop = gameHeight - spriteHeight;
-
-	var tmpStr = "";
 
 	for(var i = 0; i < Math.floor(Math.random() * 10) + 4; i++) {
-		var posLeft = (Math.floor(Math.random() * maxLeft)) + gamePosLeft;
-		var posTop = (Math.floor(Math.random() * maxTop)) + gamePosTop;
-
-		var treeCount = trees.length;
-		var tree = [posLeft, posTop, "tree"+treeCount];
-		trees[treeCount] = tree;
-
-		tmpStr = tmpStr + '<img id="tree' + treeCount + '" src="sprites/tree.png" style="position: absolute; left: ' + posLeft + 'px; top: ' + posTop + 'px"/>';
+		generateTree();
 	}
-
-	gameWindow.innerHTML = tmpStr;
 }
 
 function findTree(x, y) {
@@ -52,11 +126,25 @@ function findTree(x, y) {
 	return null;
 }
 
+function findTreeIndex(x, y) {
+	for(var i = 0; i < trees.length; i++) {
+		var treePosX = trees[i][0];
+		var treePosY = trees[i][1];
+		var treeName = trees[i][2];
+		//alert(treePosX + " - " + treePosY + "\n" + x + " - " + y);
+		if((x >= treePosX && x <= treePosX + 64) && (y >= treePosY && y <= treePosY + 64)) {
+			return i;
+		}
+	}
+
+	return null;
+}
+
 function spawnCharacter() {
 	var gameWindow = document.getElementById("game");
 	var posInfo = gameWindow.getBoundingClientRect();
 
-	gameWindow.innerHTML = gameWindow.innerHTML + '<div id="character" style="background: #FFF; height: 32px; width: 32px; position: absolute; left: ' + (posInfo.left + (posInfo.width / 2) - 16) + 'px; top: ' + (posInfo.top + (posInfo.height / 2) - 16) + 'px;"></span>';
+	gameWindow.innerHTML = gameWindow.innerHTML + '<div id="character" style="z-index: 2; background: #FFF; height: 32px; width: 32px; position: absolute; left: ' + (posInfo.left + (posInfo.width / 2) - 16) + 'px; top: ' + (posInfo.top + (posInfo.height / 2) - 16) + 'px;"></span>';
 }
 
 $(document).keydown(function(event) {
@@ -65,6 +153,7 @@ $(document).keydown(function(event) {
 	var keyUp = 38;
 	var keyDown = 40;
 	var keySpace = 32;
+	var keyB = 66;
 
 	var character = document.getElementById("character");
 	var characterPosLeft = character.style.left;
@@ -91,10 +180,22 @@ $(document).keydown(function(event) {
 			var characterPosX = character.style.left;
 			var characterPosY = character.style.top;
 			var tree = findTree(parseInt(characterPosX.replace("px", "")), parseInt(characterPosY.replace("px", "")));
-			if(tree) {
+			var treeIndex = findTreeIndex(parseInt(characterPosX.replace("px", "")), parseInt(characterPosY.replace("px", "")));
+			if(tree && treeIndex != null) {
 				var currentTree = document.getElementById(tree);
 				currentTree.remove();
+				trees.splice(treeIndex, 1);
 				updatePoints(1);
+				setTimeout(generateTree, 2500);
+			}
+		break;
+
+		case keyB:
+			if(points >= 25) {
+				alert("You have built a boat!");
+				updatePoints(-25);
+			} else {
+				alert("You need 25 wood to build a boat!");
 			}
 		break;
 	}
